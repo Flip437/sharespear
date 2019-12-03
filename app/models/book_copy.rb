5c5a6269c05628146755968d8e514e895dd99865
ArthurWilbrod <comptwil@gmail.com>
2019-12-03 15:22:18 +0100

    seed et validate

--- app/models/book_copy.rb
+++ app/models/book_copy.rb
@@ -1,3 +1,11 @@
 class BookCopy < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { minimum: 1 }
+  validates :author, presence: true, length: { minimum: 2 }
+  validates :description, presence: true, length: { maximum: 400 }
+  validates :status, inclusion: { in: [ true, false ] }
+  validates :category, presence: true
+  validates :user_id, presence: true
+
 end
