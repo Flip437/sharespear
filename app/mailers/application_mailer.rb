ce29139dfc7a5f84037229657d4322728a993552
Flip <philippediollot@hotmail.fr>
2019-12-06 16:55:27 +0100

    MAILJET WORKING git add .git add .git add .git add .git add .git add .git add .!

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@shakespeare.com'
+  default from: 'philippediollot@hotmail.fr'
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,13 @@
 class UserMailer < ApplicationMailer
+  require 'mailjet'
 
   def welcome_email(user)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user 
-
-    #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url  = 'http://monsite.fr/login' 
-
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Bienvenue chez nous !')
-
-    # mail(to: @to, from: @from, subject: "Demande d'inscription") do |format|
-    #   format.html
-    # end
-
+    #mail(to: @user.email, subject: 'Bienvenue chez nous !')
+    mail(to: @user.email, subject: "Demande d'inscription") do |format|
+      format.html
+    end
   end
 
   # def borrow_asking_email(borrow)
