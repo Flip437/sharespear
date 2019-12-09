9f14d00b9d78aafdadaf28b823f18846e0a2a89e
ArthurWilbrod <comptwil@gmail.com>
2019-12-09 14:18:50 +0100

    ajout de avatar

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :home, only: [:index]
   resources :user do
     resources :dashboard, only: [:index]
+    resources :avatars, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :bookcopy do
