3de7a2ce61aab022c45ccb3b58081c4be86cd7fa
Flip <philippediollot@hotmail.fr>
2019-12-09 14:01:17 +0100

    full user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,30 +4,28 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user 
     @url  = 'http://monsite.fr/login' 
-    mail(to: @user.email, subject: "Bienvenue sur Sharespeare !") do |format|
-      format.html
-    end
+    mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
 
-  # def borrow_asking_email(borrow)
-  #   @borrow = borrow
-  #   @borrower = User.find(@borrow.user_id)
-  #   @owner = User.find(BookCopy.find(@borrow.book_copy_id).user_id)
-  #   mail(to: @owner.email, subject: "Nouvel demande d'emprunt")
-  # end
+  def borrow_asking_email(borrow)
+    @borrow = borrow
+    @borrower = User.find(@borrow.user_id)
+    @owner = User.find(BookCopy.find(@borrow.book_copy_id).user_id)
+    mail(to: @owner.email, subject: "Nouvel demande d'emprunt")
+  end
 
-  # def borrow_accepted_email(borrow)
-  #   @borrow = borrow
-  #   @borrower = User.find(@borrow.user_id)
-  #   @owner = User.find(BookCopy.find(@borrow.book_copy_id).user_id)
-  #   mail(to: @borrower.email, subject: "Emprunt accepté !")
-  # end
+  def borrow_accepted_email(borrow)
+    @borrow = borrow
+    @borrower = User.find(@borrow.user_id)
+    @owner = User.find(BookCopy.find(@borrow.book_copy_id).user_id)
+    mail(to: @borrower.email, subject: "Emprunt accepté !")
+  end
 
-  # def borrow_declined_email(borrow)
-  #   @borrow = borrow
-  #   @borrower = User.find(@borrow.user_id)
-  #   @owner = User.find(BookCopy.find(@borrow.book_copy_id).user_id)
-  #   mail(to: @borrower.email, subject: "Emprunt refusé") 
-  # end
+  def borrow_declined_email(borrow)
+    @borrow = borrow
+    @borrower = User.find(@borrow.user_id)
+    @owner = User.find(BookCopy.find(@borrow.book_copy_id).user_id)
+    mail(to: @borrower.email, subject: "Emprunt refusé") 
+  end
 
 end
