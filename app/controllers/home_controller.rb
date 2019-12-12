a435ea297879f267511d628acf5a9f4de404cd02
Flip <philippediollot@hotmail.fr>
2019-12-12 10:48:15 +0100

    stripe first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,31 @@
 class HomeController < ApplicationController
 
+  def new
+    puts "IN NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+  end
+  
+  def create
+    # Amount in cents
+    @amount = 500
+  
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+  
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'usd',
+    })
+  
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+
+
   def index
 
     if current_user
