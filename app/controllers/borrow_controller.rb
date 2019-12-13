59c24b95ae847b3130de08a9d620bd2089a39838
Flip <philippediollot@hotmail.fr>
2019-12-13 12:21:20 +0100

    saving master

--- app/controllers/borrow_controller.rb
+++ app/controllers/borrow_controller.rb
@@ -1,77 +1,77 @@
 class BorrowController < ApplicationController
-    before_action :authenticate_user!
-    def new
-        @borrow = Borrow.new
-        @book = BookCopy.find(params[:bookcopy_id])
-    end
+  before_action :authenticate_user!
+  def new
+      @borrow = Borrow.new
+      @book = BookCopy.find(params[:bookcopy_id])
+  end
 
-    def create
-        @borrow = Borrow.new(borrow_params)
-        @borrow.user_id = current_user.id
-        @borrow.start_date = Date.today
-        duree = params[:borrow][:duree]
-        @borrow.end_date = Date.today >> duree.to_i
-        @borrow.borrow_status = 0
-        @borrow.book_copy_id = params[:bookcopy_id]
+  def create
+      @borrow = Borrow.new(borrow_params)
+      @borrow.user_id = current_user.id
+      @borrow.start_date = Date.today
+      duree = params[:borrow][:duree]
+      @borrow.end_date = Date.today >> duree.to_i
+      @borrow.borrow_status = 0
+      @borrow.book_copy_id = params[:bookcopy_id]
 
-        if @borrow.save
-            flash[:success] = "Borrow successfully created"
-            redirect_to root_path
-        else
-            puts @borrow.errors
-            flash[:error] = "Sorry, you're borrow didn't work"
-            redirect_to root_path
-        end
-
-    end
-
-    def update
-      bouton_value= params[:bouton_value]
-
-      # Demande de pret refuse
-      if bouton_value=='0'
-        borrow_to_update = Borrow.find(params[:id])
-        borrow_to_update.update(borrow_status:1)
-        #UserMailer.borrow_declined_email(borrow_to_update).deliver_now
-
-      # Demande de pret accepte
-      elsif bouton_value=='1'
-
-        borrow_to_update = Borrow.find(params[:id])
-        borrow_to_update.update(borrow_status:2)
-        UserMailer.borrow_accepted_email(borrow_to_update).deliver_now
-        book_copy_status_update = BookCopy.find(params[:bookcopy_id])
-        book_copy_status_update.update(status: false)
-
-        #passe à refusé tous les autres
-        book_copy = BookCopy.find(borrow_to_update.book_copy_id)
-        book_borrow_tab = book_copy.borrow_status_0?
-        book_borrow_tab.each do |n|
-          n.update(borrow_status:1)
-        end
-
-      # Livre recupere
-      elsif bouton_value=='2'
-        borrow_to_update = Borrow.find(params[:id])
-        book_copy_status_update = BookCopy.find(params[:bookcopy_id])
-        borrow_to_update.update(borrow_status:3)
-        book_copy_status_update.update(status: true)
-        #UserMailer.borrow_book_rendered_email(borrow_to_update).deliver_now
+      if @borrow.save
+          flash[:success] = "Borrow successfully created"
+          redirect_to root_path
+      else
+          puts @borrow.errors
+          flash[:error] = "Sorry, you're borrow didn't work"
+          redirect_to root_path
       end
 
-      redirect_to user_dashboard_index_path(current_user)
+  end
 
+  def update
+    bouton_value= params[:bouton_value]
+
+    # Demande de pret refuse
+    if bouton_value=='0'
+      borrow_to_update = Borrow.find(params[:id])
+      borrow_to_update.update(borrow_status:1)
+      UserMailer.borrow_declined_email(borrow_to_update).deliver_now
+
+    # Demande de pret accepte
+    elsif bouton_value=='1'
+
+      borrow_to_update = Borrow.find(params[:id])
+      borrow_to_update.update(borrow_status:2)
+      UserMailer.borrow_accepted_email(borrow_to_update).deliver_now
+      book_copy_status_update = BookCopy.find(params[:bookcopy_id])
+      book_copy_status_update.update(status: false)
+
+      #passe à refusé tous les autres
+      book_copy = BookCopy.find(borrow_to_update.book_copy_id)
+      book_borrow_tab = book_copy.borrow_status_0?
+      book_borrow_tab.each do |n|
+        n.update(borrow_status:1)
+      end
+
+    # Livre recupere
+    elsif bouton_value=='2'
+      borrow_to_update = Borrow.find(params[:id])
+      book_copy_status_update = BookCopy.find(params[:bookcopy_id])
+      borrow_to_update.update(borrow_status:3)
+      book_copy_status_update.update(status: true)
+      UserMailer.borrow_book_rendered_email(borrow_to_update).deliver_now
     end
 
-    def show
-      @borrow_to_show = Borrow.find(params[:id])
-      @book_to_show = BookCopy.find(params[:bookcopy_id])
-      @user_preteur = User.find(@book_to_show.user_id)
-      @user_emprunteur = User.find(@borrow_to_show.user_id)
-    end
+    redirect_to user_dashboard_index_path(current_user)
 
-    def borrow_params
-        params.require(:borrow).permit(:message)
-    end
+  end
+
+  def show
+    @borrow_to_show = Borrow.find(params[:id])
+    @book_to_show = BookCopy.find(params[:bookcopy_id])
+    @user_preteur = User.find(@book_to_show.user_id)
+    @user_emprunteur = User.find(@borrow_to_show.user_id)
+  end
+
+  def borrow_params
+      params.require(:borrow).permit(:message)
+  end
 
 end
--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,7 +1,7 @@
-Mailjet.configure do |config|
-  config.api_key = ENV['MAILJET_LOGIN']
-  config.secret_key = ENV['MAILJET_PWD']
-  config.default_from = 'philippediollot@hotmail.fr'
-  #config.default_from = 'in-v3.mailjet.com'
-  config.api_version = 'v3.1'
-end
+# Mailjet.configure do |config|
+#   config.api_key = ENV['MAILJET_LOGIN']
+#   config.secret_key = ENV['MAILJET_PWD']
+#   config.default_from = 'philippediollot@hotmail.fr'
+#   #config.default_from = 'in-v3.mailjet.com'
+#   config.api_version = 'v3.1'
+# end
