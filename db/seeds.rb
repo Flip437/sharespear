7e97b954735d8d026d44c563a83681d19350be35
ArthurWilbrod <comptwil@gmail.com>
2019-12-13 09:42:23 +0100

    ajout seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,80 @@ DatabaseCleaner.clean
 
 url_photo_link= "http://books.google.com/books/content?id=1IyauAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
 
+@isbn1=['2212563833','9782864973423','9782870972816','9791027607747','9782266305501','9782075093866','9782203172678','9782266229487','9782290020203','9782266278362','9782266258968']
+
+def create_book(isbn, index)
+
+  new_book_copy = BookCopy.new
+
+  isbn = isbn.gsub(/[.\s]/, '')
+  puts isbn
+
+  url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn.to_s
+  puts url
+  doc=JSON.load(open(url, 'User-Agent' => 'ruby'))
+  puts doc
+
+  if doc["totalItems"]==0
+    puts "ICICIC"
+      book_infos = -1
+  else
+
+      book_infos = doc['items'][0]['volumeInfo']
+
+      if book_infos["description"]
+        if book_infos["description"].length > 400
+            book_infos["description"]=book_infos["description"].slice(1..300)
+        end
+      end
+
+      if book_infos['imageLinks']
+        photo = book_infos['imageLinks']['thumbnail']
+      else
+        photo="http://books.google.com/books/content?id=1IyauAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+      end
+  
+      if  book_infos["categories"]==nil
+        category = "other"
+      else
+        category = book_infos["categories"][0]
+      end
+      if  book_infos["description"]==nil
+        description = "laisse toi tenter"
+      else
+        description = book_infos["description"]
+      end
+      if  book_infos["authors"]==nil
+        author = "no info"
+      else
+        author = book_infos["authors"][0]
+      end
+      if  book_infos["title"]==nil
+        title = "no title"
+      else
+        title = book_infos["title"]
+      end
+
+      new_book_copy = BookCopy.create(
+        title:  title,
+        author: author,
+        description: description,
+        status: true,
+        category: category,
+        user_id: index,
+        photo_link: photo,
+        isbn: isbn
+      )
+
+      puts new_book_copy.inspect
+
+      return new_book_copy
+
+    end
+
+end
+
+
 User.destroy_all
 10.times.with_index do |user, index|
     user = User.create(
@@ -50,21 +124,15 @@ User.destroy_all
     puts "User #{index} created"
 end
 
+
+
 BookCopy.destroy_all
 9.times do |index|
 
   #5 livre dispo
-  5.times do |index2|
-      book_copy = BookCopy.create(
-          title: Faker::Book.title,
-          author: Faker::Book.author,
-          description: Faker::TvShows::SiliconValley.quote,
-          status: true, # [true, false].sample,
-          category: Faker::Book.genre,
-          isbn: "102020200",
-          photo_link: url_photo_link,
-          user_id: index+1
-      )
+  create_book(@isbn1[index], index+1)
+  2.times do |index2|
+      #create_book(@isbn1[index2], index+1)
   end
   puts "User #{index+1} has 5 book_copy dispo"
   #5 livre dispo en demande d'emprunt par utilisateur par utilisateur +1
