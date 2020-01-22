a7c1958b60731986fb1b166c3f65bfc20b5490a7
Flip <philippediollot@hotmail.fr>
2020-01-22 17:50:15 +0100

    gmail sender OK

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,23 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# ActionMailer::Base.smtp_settings = {
+#     :user_name => ENV['SENDGRID_LOGIN'],
+#     :password => ENV['SENDGRID_API_KEY'],
+#     :domain => 'sharespeare.fr',
+#     :address => 'smtp.sendgrid.net',
+#     :port => 465,
+#     :authentication => :plain,
+#     :enable_starttls_auto => true,
+#     :tls => true
+# }
+
+# SMTP settings for gmail
 ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_LOGIN'],
-    :password => ENV['SENDGRID_API_KEY'],
-    :domain => 'sharespeare.fr',
-    :address => 'smtp.sendgrid.net',
-    :port => 465,
-    :authentication => :plain,
-    :enable_starttls_auto => true,
-    :tls => true
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => ENV['GMAIL_LOGIN'],
+  :password             => ENV['GMAIL_PASSWORD'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
 }
