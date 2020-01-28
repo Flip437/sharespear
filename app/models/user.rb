da9a108be7ca5d341a9a7f53e59512e3ac7c5dce
Flip <flip@flip-hp-ah0xxx.tuba.guest>
2020-01-28 17:03:07 +0100

    need to restore devise mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  after_create :welcome_send
+  after_create :confirmation_instructions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :book_copies
   has_many :borrows
@@ -25,6 +25,10 @@ class User < ApplicationRecord
    !deleted_at ? super : :deleted_account
  end  
 
+  def confirmation_instructions
+    UserMailer.confirmation_instructions(self).deliver_now
+  end
+ 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
