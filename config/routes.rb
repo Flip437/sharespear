32d3156feab8bb5fa6a6885da1f74499a95a9abd
Flip <philippediollot@hotmail.fr>
2020-02-03 13:13:01 +0100

    adding gem active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get 'comments/New'
   get 'comments/Create'
   get 'mailback/new'
