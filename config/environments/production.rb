dd8b9caab4fecf4d3b0606f444acf5e6b8fb5865
ArthurWilbrod <comptwil@gmail.com>
2020-02-06 09:56:12 +0100

    aa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "http://gmail.com'" }
