b0072b23b5df8806cb051f883e7a66430d8785dd
Flip <philippediollot@hotmail.fr>
2020-02-10 17:09:46 +0100

    confirmation email OK

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-ActiveSupport::Reloader.to_prepare do
-  ApplicationController.renderer.defaults.merge!(
-    http_host: 'https://sharespeare.herokuapp.com/',
-    https: false
-  )
-end
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
