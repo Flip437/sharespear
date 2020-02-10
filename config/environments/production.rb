c40a133d9d9637c4c544bc8b9597868165f2619a
Flip <philippediollot@hotmail.fr>
2020-02-10 17:05:53 +0100

    configure email confiramtion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,5 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://sharespeare.herokuapp.com/'" }
+  config.action_mailer.default_url_options = { host: "https://sharespeare.herokuapp.com" }
 end
