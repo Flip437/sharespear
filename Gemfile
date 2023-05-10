e25531da046b06d6a3f13f396524ddbd46f38fd2
flip <philippediollot@hotmail.fr>
2023-05-10 13:28:42 +0200

    rails s working

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.5.1'
+ruby '2.6.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
@@ -84,3 +84,4 @@ gem "google-cloud-storage", "~> 1.8", require: false
 gem 'activeadmin'
 gem 'social-share-button', '~> 0.1.6'
 gem 'meta-tags'
+gem "sassc-rails"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_133312) do
+ActiveRecord::Schema.define(version: 2023_03_11_105912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,14 @@ ActiveRecord::Schema.define(version: 2020_02_11_133312) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_book_copies_on_user_id"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "summary"
+    t.integer "rate"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "borrows", force: :cascade do |t|
